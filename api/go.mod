bf4ca57b2c52dfc11207496e84c4e74829dd4ff0
CH3CHO <kentstl@163.com>
2021-01-10 10:20:50 +0800

    Implement api.

--- api/go.mod
+++ api/go.mod
@@ -3,6 +3,7 @@ module github.com/CH3CHO/GoMultiModuleTest/api
 go 1.14
 
 require (
+	github.com/CH3CHO/GoMultiModuleTest/core v0.0.0-20210110021328-158f731ff549
 	github.com/gin-gonic/gin v1.6.3
 	github.com/go-playground/validator/v10 v10.4.1 // indirect
 	github.com/golang/protobuf v1.4.3 // indirect
--- api/main.go
+++ api/main.go
@@ -2,16 +2,18 @@ package main
 
 import (
 	"fmt"
+	"github.com/CH3CHO/GoMultiModuleTest/core/service"
 	"github.com/gin-gonic/gin"
 	"github.com/golobby/container/pkg/container"
 	"net/http"
-	"github.com/CH3CHO/GoMultiModuleTest/core/service"
 )
 
-var ctn = &container.Container{}
+var sayHelloService service.SayHelloService
 
 func init() {
+	ctn := &container.Container{}
 	service.InitContainer(ctn)
+	ctn.Make(&sayHelloService)
 }
 
 func main() {
@@ -20,12 +22,17 @@ func main() {
 		c.String(http.StatusOK, "You've reached test service.")
 	})
 	r.GET("/sayHello", func(c *gin.Context) {
+		name := c.Query("name")
+		if name == "" {
+			name = "nil"
+		}
+		text := sayHelloService.SayHello(name)
 		c.JSON(200, gin.H{
-			"text": "pong",
+			"text": text,
 		})
 	})
 	err := r.Run()
 	if err != nil {
-		fmt.Printf("%v", err)
+		fmt.Printf("Failed to start server: %v", err)
 	}
 }
